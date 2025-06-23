787c2cdd55dce5c3e2cdbab6c212b0f550ec8d88
agustin <agustin.bruno@davinci.edu.ar>
2025-06-23 20:40:14 -0300

    modificacion de modelo producto

--- ERPico/models/product.py
+++ ERPico/models/product.py
@@ -1,9 +1,10 @@
 
 class Product:
-    def __init__(self, name, price, stock):
+    def __init__(self, name, price, stock,descuento):
         self.name = name
         self.price = price
         self.stock = stock
+        self.descuento = descuento
 
     def __str__(self):
         return f"{self.name} - ${self.price} ({self.stock} en stock)"
