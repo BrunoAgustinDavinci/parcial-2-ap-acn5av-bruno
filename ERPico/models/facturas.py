1f2135f4798a5bf369430084397a93bfd7b06c0a
agustin <agustin.bruno@davinci.edu.ar>
2025-06-24 20:58:49 -0300

    modificacion de modelo factura

--- ERPico/models/facturas.py
+++ ERPico/models/facturas.py
@@ -1,10 +1,11 @@
 
 class Facturas:
-    def __init__(self, name, price, stock,descuento):
+    def __init__(self, name, price, stock,descuento,factura,ticket):
         self.name = name
         self.price = price
         self.stock = stock
         self.descuento = descuento
-
+        self.factura = factura
+        self.ticket = ticket
     def __str__(self):
         return f"{self.name} - ${self.price} ({self.stock} en stock)"
