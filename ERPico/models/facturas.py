914d6c6f08f328908986353ae0df90123aac4054
agustin <agustin.bruno@davinci.edu.ar>
2025-06-24 20:58:18 -0300

    creacion de modelo

---
+++ ERPico/models/facturas.py
@@ -0,0 +1,10 @@
+
+class Facturas:
+    def __init__(self, name, price, stock,descuento):
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.descuento = descuento
+
+    def __str__(self):
+        return f"{self.name} - ${self.price} ({self.stock} en stock)"
